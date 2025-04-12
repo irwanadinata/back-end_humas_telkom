5e17da75e15b96a22bbe85cfac9942e643e4f9f8
irwanadinata <irwanadinata9@gmail.com>
2025-04-12 10:06:16 +0700

    create: multer middleware

---
+++ middleware/multer.middleware.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadFolder = "uploads";
+if (!fs.existsSync(uploadFolder)) {
+  fs.mkdirSync(uploadFolder);
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadFolder);
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + file.originalname.replace(/\s+/g, "_");
+    cb(null, uniqueName);
+  },
+});
+
+const upload = multer({ storage });
+export default upload;
