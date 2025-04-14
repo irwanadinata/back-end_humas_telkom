081b9d481b38118a1e31b4e518c1aa3129a4f738
irwanadinata <irwanadinata9@gmail.com>
2025-04-14 19:27:27 +0700

    refactor: auth middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import roleAccess from "./role-access.js";
 
 const auth = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
@@ -7,6 +8,15 @@ const auth = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
+
+    const endpoint = req.baseUrl;
+    const method = req.method;
+
+    const isAllowed = roleAccess(decoded.role, endpoint, method);
+    if (!isAllowed) {
+      return res.status(403).json({ message: "Akses ditolak" });
+    }
+
     next();
   } catch (err) {
     return res.status(401).json({ message: "Token tidak valid" });
